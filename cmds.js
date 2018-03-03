caa7ab097942c2779cc434c4101978ebeaa34583
Ignacio Mata Alonso <ignacio.mata.alonso@alumnos.upm.es>
2018-03-03 11:44:11 +0100

    Primera versión

---
+++ cmds.js
@@ -0,0 +1,139 @@
+const {log, biglog, errorlog, colorize} = require("./out");
+const model = require('./model');
+
+exports.helpCmd = rl => {
+    log("Commandos:");
+    log(" h|help - Muestra esta ayuda.");
+    log(" list - Listar los quizzes existentes.");
+    log(" show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
+    log(" add - Añadir un nuevo quiz interactivamente.");
+    log(" delete <id> - Borrar el quiz indicado.");
+    log(" edit <id> - Editar el quiz indicado.");
+    log(" test <id> - Probar el quiz indicado.");
+    log(" p|play -Jugar a preguntar aleatoriamente todos los quizzes.");
+    log(" credits - Créditos.");
+    log(" q|quit - Salir del programa.");
+    rl.prompt();
+};
+
+exports.listCmd = rl => {
+
+    model.getAll().forEach((quiz, id) => {
+
+        log(` [${colorize(id, 'magenta')}]: ${quiz.question}`);
+    });
+
+    rl.prompt();
+};
+
+exports.showCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+            log(` [${colorize(id, 'magenta')}]: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`);
+        } catch(error) {
+            errorlog(error.message);
+        }
+    }
+
+    rl.prompt();
+};
+
+exports.addCmd = rl => {
+
+    rl.question(colorize(' Introduzca una pregunta: ', 'red'), question => {
+
+        rl.question(colorize(' Introduzca la respuesta ', 'red'), answer => {
+
+            model.add(question, answer);
+            log(` ${colorize('Se ha añadido', 'magenta')}: ${question} ${colorize('=>', 'magenta')} ${answer}`);
+            rl.prompt();
+        });
+    });
+};
+
+exports.deleteCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try {
+            model.deleteByIndex(id);
+        } catch(error) {
+            errorlog(error.message);
+        }
+    }
+
+    rl.prompt();
+};
+
+exports.editCmd = (rl, id) => {
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    } else {
+        try{
+            const quiz = model.getByIndex(id);
+
+            process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)},0);
+
+            rl.question(colorize(' Introduzca una pregunta: ', 'red'), question => {
+
+                process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)},0);
+
+                rl.question(colorize(' Introduzca la respuesta ', 'red'), answer => {
+                    model.update(id, question, answer);
+                    log(` Se ha cambiado el quiz ${colorize(id, 'magenta')} por : ${question} ${colorize('=>', 'magenta')} ${answer}`);
+                    rl.prompt();
+                });
+            });
+        } catch(error) {
+            errorlog(error.message);
+            rl.prompt();
+        }
+    }
+};
+
+exports.testCmd = (rl, id) => {
+
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+            rl.question(` ${colorize(quiz.question,'red')}? `, answer => {
+                let resp = answer.toLowerCase().trim();
+                if (resp === quiz.answer.toLowerCase().trim()) {
+                    biglog('CORRECTO', 'green');
+                    rl.prompt();
+                } else {
+                    biglog('INCORRECTO', 'red');
+                    rl.prompt();
+                }
+            });
+
+        } catch(error) {
+            errorlog(error.message);
+            rl.prompt();
+        }
+    }
+};
+
+exports.playCmd = rl => {
+    log('Jugar.', 'red');
+    rl.prompt();
+};
+
+exports.creditsCmd = rl => {
+    log('Autores de la práctica:');
+    log('Ignacio Mata Alonso', 'green');
+    rl.prompt();
+};
+
+exports.quitCmd = rl => {
+    rl.close();
+};
